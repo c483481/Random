7e1462116832c49ed744b3be59a054f7e0daa037
c483481 <charleskojansow@gmail.com>
2024-05-30 19:53:22 +0800

    feat(rust): add random number benchmark test

--- rust/benches/main.rs
+++ rust/benches/main.rs
@@ -3,8 +3,10 @@ use criterion::{
     criterion_main
 };
 mod random_string;
+mod random_number;
 
 use random_string::random_string_benchmark;
+use random_number::random_number_benchmark;
 
-criterion_group!(benches, random_string_benchmark);
+criterion_group!(benches, random_string_benchmark, random_number_benchmark);
 criterion_main!(benches);
